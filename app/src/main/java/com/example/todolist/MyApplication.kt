506dac49ed5cc6ed84b705daf161f3315e24d7f0
panliang-rm <1970340407@qq.com>
2021-09-20 00:46:18 +0800

    增加LeanCloud服务端，做好基础配置

---
+++ app/src/main/java/com/example/todolist/MyApplication.kt
@@ -0,0 +1,20 @@
+package com.example.todolist
+
+import android.app.Application
+import cn.leancloud.LCObject
+import cn.leancloud.LeanCloud
+
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        LeanCloud.initialize(
+            this,
+            "rG0YdI2JrEpnUpkJcIDbcIOS-gzGzoHsz",
+            "fwY97QeFHH3qugMiyt1toRSN",
+            "https://rg0ydi2j.lc-cn-n1-shared.com"
+        )
+
+    }
+}
