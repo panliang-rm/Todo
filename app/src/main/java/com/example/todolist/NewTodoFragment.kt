8d9202827ac0daf0f240018b62ef3b54758081cf
panliang-rm <1970340407@qq.com>
2021-09-24 14:12:37 +0800

    实现recycleview的左右滑动，左滑删除，右滑编辑

--- app/src/main/java/com/example/todolist/NewTodoFragment.kt
+++ app/src/main/java/com/example/todolist/NewTodoFragment.kt
@@ -6,25 +6,27 @@ import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
-import androidx.core.content.ContextCompat
 import cn.leancloud.LCObject
 import cn.leancloud.LCUser
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import io.reactivex.Observer
 import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.fragment_new_todo.*
-import java.util.*
 
 
 class AddNewTask : BottomSheetDialogFragment() {
+
+    private lateinit var objectsid: String
+    private lateinit var text: String
+
+    fun newInstance(): AddNewTask {
+        return AddNewTask()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,55 +36,70 @@ class AddNewTask : BottomSheetDialogFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         val view: View = inflater.inflate(R.layout.fragment_new_todo, container, false)
-        dialog!!.window!!.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+        dialog?.window
+                ?.setSoftInputMode(WindowManager
+                        .LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        var isUpdate = false
+        val bundle: Bundle? = arguments
+        if (bundle != null) {
+            isUpdate = true
+            text = bundle.getString("title").toString()
+            objectsid = bundle.getString("objectid").toString()
+            fragment_newTaskText.setText(text)
+            if (text.isNotEmpty()) {
+                frangment_newTaskButton.setTextColor(resources.getColor(R.color.wenzi))
+            }
+        }
+
         fragment_newTaskText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
                 if (s.toString() == "") {
-                    fragment_newTaskText?.isEnabled = false
-                    fragment_newTaskText.setTextColor(Color.GRAY)
+                    frangment_newTaskButton?.isEnabled = false
+                    frangment_newTaskButton.setTextColor(Color.GRAY)
                 } else {
-                    fragment_newTaskText?.isEnabled = true
-                    fragment_newTaskText.setTextColor(ContextCompat.getColor(Objects.requireNonNull(context)!!, R.color.colorPrimaryDark))
+                    frangment_newTaskButton?.isEnabled = true
+                    frangment_newTaskButton.setTextColor(resources.getColor(R.color.wenzi))
                 }
             }
-
             override fun afterTextChanged(s: Editable) {}
         })
+
         frangment_newTaskButton.setOnClickListener{
             val title = fragment_newTaskText.text.toString()
             if (title.isNotEmpty()) {
-                // 构建对象
-                val todo = LCObject("Todo")
-                // 为属性赋值
-                todo.put("title", title)
-                todo.put("isChecked", false)
-                todo.put("user", LCUser.getCurrentUser())
-                // 将对象保存到云端
-                todo.saveInBackground().subscribe(object : Observer<LCObject?> {
-                    override fun onSubscribe(d: Disposable) {
-                    }
+                if (isUpdate) {
+                    Thread{
+                        val todo = LCObject.createWithoutData("Todo", objectsid)
+                        todo.put("title", title)
+                        todo.save()
+                    }.start()
+                } else {
+                    // 构建对象
+                    val todo = LCObject("Todo")
+                    // 为属性赋值
+                    todo.put("title", title)
+                    todo.put("isChecked", false)
+                    todo.put("user", LCUser.getCurrentUser())
+                    // 将对象保存到云端
+                    todo.saveInBackground().subscribe(object : Observer<LCObject?> {
+                        override fun onSubscribe(d: Disposable) {}
+                        override fun onNext(t: LCObject) {}
+                        override fun onError(e: Throwable) {
+                            Toast.makeText(context, e.toString(), Toast.LENGTH_SHORT).show()
+                        }
 
-                    override fun onNext(t: LCObject) {
-                        //val _todo = Todo(t.getString("title"), t.getBoolean("isChecked"), t.objectId)
-                    }
-
-                    override fun onError(e: Throwable) {
-                        Toast.makeText(context, e.toString(), Toast.LENGTH_SHORT).show()
-                    }
-
-                    override fun onComplete() {
-                    }
-                })
+                        override fun onComplete() {}
+                    })
+                }
                 dismiss()
             }
-
         }
     }
 
